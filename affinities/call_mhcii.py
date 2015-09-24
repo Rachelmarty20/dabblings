4e91a070bec9bfc0fa69b5006cf75381e1f6ca4a
Rachelmarty20 <Rachelmarty20@gmail.com>
2015-09-24 11:52:58 -0700

    take two

--- affinities/call_mhcii.py
+++ affinities/call_mhcii.py
@@ -11,6 +11,9 @@ affinities_directory = '/data/nrnb01/ramarty/hla/affinities_random_ii'
 with open(file_mhc_alleles) as f:
     mhc_alleles = [allele.strip() for allele in f.readlines()]
 
+mut_dir = '/data/nrnb01/ramarty/hla/affinities_random_ii/{0}'.format(mutation)
+os.mkdir(mut_dir)
+
 allele = mhc_alleles[0]
 cmd = '/cellar/users/ramarty/programs/mhc_i/src/predict_binding.py IEDB_recommended {0} /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa > /data/nrnb01/ramarty/hla/affinities_random_ii/{1}/all.affinities'.format(allele, mutation)
 os.system(cmd)
