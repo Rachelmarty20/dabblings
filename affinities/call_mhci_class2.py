65f0275130a8acda437d7301c99724f501d3fd5b
Rachelmarty20 <Rachelmarty20@gmail.com>
2015-09-24 10:12:56 -0700

    creating directory

--- affinities/call_mhci_class2.py
+++ affinities/call_mhci_class2.py
@@ -12,6 +12,9 @@ affinities_directory = '/data/nrnb01/ramarty/hla/affinities_random_ii'
 with open(file_mhc_alleles) as f:
     mhc_alleles = [allele.strip() for allele in f.readlines()]
 
+cmd = 'mkdir /data/nrnb01/ramarty/hla/affinities_random_ii/{0}/all.affinities'.format(mutation)
+os.system(cmd)
+
 print "trying"
 allele = mhc_alleles[0]
 cmd = '/cellar/users/ramarty/programs/mhc_ii/mhc_II_binding.py IEDB_recommended {0} /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa > /data/nrnb01/ramarty/hla/affinities_random_ii/{1}/all.affinities'.format(allele, mutation)
