0271984460e2f5f12432ec09b862e923a7a5ea4d
Rachelmarty20 <Rachelmarty20@gmail.com>
2015-09-25 14:14:06 -0700

    updates

--- affinities/old.call_mhcii.py
+++ affinities/old.call_mhcii.py
@@ -13,11 +13,13 @@ affinities_directory = '/data/nrnb01/ramarty/hla/affinities_random_ii'
 with open(file_mhc_alleles) as f:
     mhc_alleles = [allele.strip() for allele in f.readlines()]
 
-
 allele = mhc_alleles[0]
-cmd = "python /cellar/users/ramarty/programs/mhc_ii/mhc_II_binding.py consensus3 {0} /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa > /data/nrnb01/ramarty/hla/affinities_random_ii/{1}/all.affinities".format(allele, mutation)
-print cmd
 
-for allele in mhc_alleles[1:]:
-    cmd = "python /cellar/users/ramarty/programs/mhc_ii/mhc_II_binding.py consensus3 {0} /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa >> /data/nrnb01/ramarty/hla/affinities_random_ii/{1}/all.affinities".format(allele, mutation)
-    print cmd
+with open("/data/nrnb01/ramarty/hla/affinities_random_ii/{0}/all.affinities".format(mutation)) as outfile:
+
+    cmd = "python /cellar/users/ramarty/programs/mhc_ii/mhc_II_binding.py consensus3 {0} /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa".format(allele, mutation)
+    outfile.write(cmd+"\n")
+
+    for allele in mhc_alleles[1:]:
+        cmd = "python /cellar/users/ramarty/programs/mhc_ii/mhc_II_binding.py consensus3 {0} /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa".format(allele, mutation)
+        outfile.write(cmd+"\n")
