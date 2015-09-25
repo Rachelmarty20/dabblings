46f50b12952d9a8c4059f720311cc86149cc2e20
Rachelmarty20 <Rachelmarty20@gmail.com>
2015-09-25 12:10:18 -0700

    updates

--- affinities/call_mhcii.py
+++ affinities/call_mhcii.py
@@ -19,26 +19,14 @@ try:
 except:
     print "directory exists"
 
-print "/data/nrnb01/ramarty/hla/affinities_random_ii/{0}/all.affinities".format(mutation)
-with open("/data/nrnb01/ramarty/hla/affinities_random_ii/{0}/all.affinities".format(mutation), 'w') as outfile:
-    allele = mhc_alleles[0]
+output = []
+for allele in mhc_alleles:
     cmd = "/cellar/users/ramarty/programs/mhc_ii/mhc_II_binding.py consensus3 {0} /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa".format(allele, mutation)
     # IEDB_recommended '{0}' /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa > /data/nrnb01/ramarty/hla/affinities_random_ii/{1}/all.affinities".format(allele, mutation)"
     cmd_list = cmd.split()
-    print cmd_list
-    s1= subprocess.check_output(cmd_list)
-    print 'call number 1: ',subprocess.call(cmd_list, stdout=outfile)
+    output.append(subprocess.check_output(cmd_list))
 
 
-    cmd = "/cellar/users/ramarty/anaconda/bin/python /cellar/users/ramarty/programs/mhc_ii/mhc_II_binding.py consensus3 '{0}' /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa".format(allele, mutation)
-    cmd_list = cmd.split()
-    print cmd_list
-    
-    print 'call number 2: ',subprocess.call(cmd_list, stdout=outfile,shell=True)
-
-    print "one done"
-
-    for allele in mhc_alleles[1:]:
-        cmd = '/cellar/users/ramarty/anaconda/bin/python /cellar/users/ramarty/programs/mhc_ii/mhc_II_binding.py consensus3 {0} /cellar/users/ramarty/Projects/hla_new/data/mutations/fasta_files/random/{1}.fsa'.format(allele, mutation)
-        cmd_list = cmd.split()
-        print 'call number 3:',subprocess.call(cmd_list, stdout=outfile,shell=True)
+#print "/data/nrnb01/ramarty/hla/affinities_random_ii/{0}/all.affinities".format(mutation)
+with open("/data/nrnb01/ramarty/hla/affinities_random_ii/{0}/all.affinities".format(mutation), 'w') as outfile:
+    outfile.write("\n".join(output))
